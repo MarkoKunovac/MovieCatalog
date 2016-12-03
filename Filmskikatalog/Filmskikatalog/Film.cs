7f1bcaf22782d286765727b623923d9ccd7f150d
Marko <kunovac_marko@yahoo.com>
2016-12-03 14:04:46 +0100

    pocetak kataloga

---
+++ Filmskikatalog/Filmskikatalog/Film.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmskikatalog
+{
+    class Film
+    {
+        public string Name { get; set;}
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string ReleaseDate { get; set; }
+        public static List<Film> getMovie()
+        {
+            var movie = new List<Film>();
+            movie.Add(new Film() { Name = "Avatar", Genre = "Fantasy", Director = "James Cameron", ReleaseDate = "12/18/2009 12:00:00 AM" });
+            movie.Add(new Film() { Name = "Dark Knight", Genre = "Action", Director = "Christopher Nolan", ReleaseDate = "7/18/2008 12:00:00 AM" });
+            movie.Add(new Film() { Name = "Gilmors girls", Genre = "Sci-Fi", Director = "Ivan Peric", ReleaseDate = "1/1/2018 12:00:00 AM"});
+            movie.Add(new Film() { Name = "Scrubs", Genre = "Family", Director = "Oliver Sipos", ReleaseDate = "1/1/2019 12:00:00 AM" });
+            return movie;
+        }
+    }
+}
---
+++ Filmskikatalog/Filmskikatalog/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Filmskikatalog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataGrid.ItemsSource = Film.getMovie();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            AddMovie win2 = new AddMovie();
+            win2.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure you want to exit?", "Confirmation", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
