21fc4865602061f0d7477bb3db34983b5d7a512b
DOIT_DOTNET_USER13 <DOIT_DOTNET_USER13@C13>
2016-12-06 21:49:35 +0100

    test

--- Filmskikatalog/Filmskikatalog/MainWindow.xaml.cs
+++ Filmskikatalog/Filmskikatalog/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Filmskikatalog
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
